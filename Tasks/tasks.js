f62d8ae19baca43ab3096246b8dbc55d01eb2afb
JoaoGuilherme7 <119643173+JoaoGuilherme7@users.noreply.github.com>
2023-11-10 17:58:59 -0300

    new version

--- Tasks/tasks.js
+++ Tasks/tasks.js
@@ -1,29 +1,69 @@
+
 document.addEventListener('DOMContentLoaded', () => {
 
-    //By default submit button is disabled
-    document.querySelector('#submit').disabled = true;
-    document.querySelector('#task').onkeyup = () => {
-        if(document.querySelector('#task').value.length > 0){
-            document.querySelector('#submit').disabled = false;
-        }else{
-            document.querySelector('#submit').disabled = true;
-        } 
-    }
 
-    const form = document.querySelector('form')
+    formatForm();
+    veryfyInputContent();
+
+    const form = document.querySelector('form');
     form.onsubmit = () => {
-        const task = document.querySelector('#task').value;
-        const li = document.createElement('li');
-        li.innerHTML = task;
-
-        document.querySelector('#tasksList').append(li);
-        document.querySelector('#task').value = '';
-        document.querySelector('#submit').disabled = true;
-
-
-
-
+        main();
         // Stop form submiting
         return false;
     }
+
 })
+function veryfyInputContent() {
+    const submitButton = document.querySelector('#submit');
+    const inputField = document.querySelector('#task');
+
+    inputField.onkeyup = () => {
+        if (inputField.value.length > 0) {
+            submitButton.disabled = false;
+        } else {
+            submitButton.disabled = true;
+        }
+    }
+}
+function formatForm() {
+    const inputField = document.querySelector('#task');
+    //CLEAR INPUT FIELD & SET SUBMIT BUTTON DISABLED
+    document.querySelector('#task').value = '';
+    document.querySelector('#submit').disabled = true;
+    inputField.focus();
+}
+function transformToLi(value) {
+    const li = ` <li>
+                    ${value}">${value}
+                </li>`;
+    return li;
+}
+function pasteTask(array) {
+    document.querySelector('#tasksList').innerHTML = array;
+}
+function getTask() {
+    const task = document.querySelector('#task').value;
+    return task;
+}
+function addToTasksArray(iten, array) {
+
+    array.push(iten);
+    return array;
+}
+
+// ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+let toDo = [];
+
+function main() {
+
+    const task = getTask()
+    addToTasksArray(task, toDo);
+    let liArray = toDo.map(transformToLi).join('');
+
+    // console.log(liArray);
+
+    pasteTask(liArray)
+    formatForm();
+
+}
+// :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
